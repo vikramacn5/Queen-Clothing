8e3ff6a1149e584b6d9b968aca2b84f6a79cc340
vikramacn5 <vikram.cn5@gmail.com>
2021-12-18 20:05:09 +0530

    84. header component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -35,6 +36,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
