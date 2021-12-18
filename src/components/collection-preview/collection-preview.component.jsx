619079c6616a3e2926cf35db2fd7fe2638417c74
vikramacn5 <vikram.cn5@gmail.com>
2021-12-18 18:10:33 +0530

    83. collection item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,11 +1,13 @@
+import CollectionItem from '../collection-item/collection-item.component';
+
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.slice(0, 4).map((item) => (
-        <div key={item.id}>{item.name}</div>
+      {items.slice(0, 4).map(({ id, ...otherItemProps }) => (
+        <CollectionItem key={id} {...otherItemProps} />
       ))}
     </div>
   </div>
