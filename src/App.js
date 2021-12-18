08d536d183d23592c55062bb12bad2704570507d
vikramacn5 <vikram.cn5@gmail.com>
2021-12-18 17:14:17 +0530

    82. shop page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 // const TestPage = () => {
 //   const location = useLocation();
@@ -25,18 +26,19 @@ import HomePage from './pages/homepage/homepage.component';
 //   );
 // }
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/shop/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        {/* <Route path="/shop/hats" element={<HatsPage />} /> */}
       </Routes>
     </div>
   );
