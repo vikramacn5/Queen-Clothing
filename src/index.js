32ac3667c276169cd2474862fd1405ae76cbc650
vikramacn5 <vikram.cn5@gmail.com>
2021-12-26 07:17:28 +0530

    144. Redux persist

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import storage from 'redux-persist/lib/storage';
 // import testReducer from './test.reducer';
 
-export default combineReducers({
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   // test: testReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+export default persistReducer(persistConfig, rootReducer);
